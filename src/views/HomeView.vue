3c43ee81a34cb4f98245e04667f3ac34dddf8c11
Alexander Ilyin <mcr1ne90@gmail.com>
2022-10-22 22:24:42 +0600

    Refactor store usage in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { useCounterStore } from "@/stores/counter";
 
 const store = useCounterStore();
+
+// we cannot destructure reactive properties from store object directly
+// without losing reactivity, that's why we need to use storeToRefs
+const { count, doubleCount } = storeToRefs(store);
+
+// actions can just be extracted
+const { increment } = store;
 </script>
 
 <template>
   <main>Hello world</main>
-  <div>Count: {{ store.count }}</div>
-  <div>Double count: {{ store.doubleCount }}</div>
+  <div>Count: {{ count }}</div>
+  <div>Double count: {{ doubleCount }}</div>
   <div>
-    <button @click="store.increment">Increment</button>
+    <button @click="increment">Increment</button>
   </div>
 </template>
