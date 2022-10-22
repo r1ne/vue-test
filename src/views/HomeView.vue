8a579def73c673db0c517df02de5f6ed4819dcc3
Alexander Ilyin <mcr1ne90@gmail.com>
2022-10-22 21:55:19 +0600

    Add simple counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+
+const store = useCounterStore();
+</script>
 
 <template>
   <main>Hello world</main>
+  <div>Count: {{ store.count }}</div>
+  <div>Double count: {{ store.doubleCount }}</div>
+  <div>
+    <button @click="store.increment">Increment</button>
+  </div>
 </template>
