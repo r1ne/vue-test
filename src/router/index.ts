8cb8f368a842bc9b10d23401707cca5d5665c9e7
Alexander Ilyin <mcr1ne90@gmail.com>
2022-10-23 12:48:23 +0600

    Add simple 404 view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import EventsView from "@/views/EventsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/events",
       name: "events",
       component: EventsView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Sorry, not found</h1>
+</template>
